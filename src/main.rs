698ab5e41513e8488bc2c36a14d2baa5c619ae02
roazanas <me@rznz.ru>
2025-11-23 15:51:11 +0500

    feat: empty parser state (logos + chumsky is connected)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,13 @@
-use std::io::{self, BufRead};
+use chumsky::Parser;
 
 mod lexer;
+mod parser;
 
 fn main() {
     env_logger::init();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        match line {
-            Ok(input) => {
-                if input.is_empty() {
-                    break;
-                }
-                lexer::tokenize(&input);
-            }
-            Err(_) => break,
-        }
-    }
+    // let test_code =
+    //     std::fs::read_to_string("/home/rznz/dev_proj/rust/pillar/example.rplr").unwrap();
+    let tokens = lexer::tokenize("");
+    let parser = parser::parser();
+    println!("{:?}", parser.parse(&tokens));
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,6 @@
-use ::chumsky::prelude::*;
+use crate::lexer::Token;
+use chumsky::prelude::*;
+
+pub fn parser<'src>() -> impl Parser<'src, &'src [Token<'src>], ()> {
+    end()
+}
