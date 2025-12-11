565ceaaa36e194298e4cc4386cb4d4d9d8d28187
roazanas <me@rznz.ru>
2025-12-11 14:07:59 +0500

    decor: add color for cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,20 @@
 use std::path::PathBuf;
 
 use clap::Parser as CliParser;
+use clap::builder::styling::{AnsiColor, Effects, Styles};
+
+const STYLES: Styles = Styles::styled()
+    .header(AnsiColor::Green.on_default().effects(Effects::BOLD))
+    .usage(AnsiColor::Green.on_default().effects(Effects::BOLD))
+    .literal(AnsiColor::Cyan.on_default().effects(Effects::BOLD))
+    .placeholder(AnsiColor::Cyan.on_default());
 
 #[derive(Debug, CliParser)]
-#[command(version, about = "Compiler for vertical esolang pillar")]
+#[command(
+    version, 
+    about = "Compiler for vertical esolang pillar",
+    styles = STYLES,
+)]
 pub struct Args {
     pub file: PathBuf,
     #[arg(short, long, default_value = "out")]
