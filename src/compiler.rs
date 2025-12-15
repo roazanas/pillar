416a08d893d8d968c87482c6bba955568b6de8e4
roazanas <me@rznz.ru>
2025-12-15 19:49:45 +0500

    refactor: major compiler refactor and fixes

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,7 +1,7 @@
-use cranelift::prelude::InstBuilder;
 use std::collections::HashMap;
 
-use cranelift::{codegen::Context, prelude::*};
+use cranelift::codegen::Context;
+use cranelift::prelude::*;
 use cranelift_module::{FuncId, Linkage, Module};
 use owo_colors::OwoColorize;
 
@@ -55,18 +55,16 @@ impl IRCompiler {
         code: Block,
     ) -> Result<FuncId, String> {
         let mut sig = module.make_signature();
-
         for &param in arguments {
             sig.params.push(AbiParam::new(param));
         }
-
         if let Some(ret) = return_type {
             sig.returns.push(AbiParam::new(ret));
         }
 
         let func_id = module
             .declare_function(name, Linkage::Export, &sig)
-            .map_err(|e| format!("{} {}", "Unable to declare function:".red().bold(), e))?;
+            .map_err(|e| format!("Unable to declare function: {e}"))?;
 
         let mut ctx = module.make_context();
         ctx.func.signature = sig;
@@ -77,153 +75,162 @@ impl IRCompiler {
         builder.switch_to_block(entry_block);
         builder.seal_block(entry_block);
 
-        let mut next_var = 0usize;
-        let mut variables: HashMap<String, Variable> = HashMap::new();
+        let mut function_compiler = FunctionCompiler {
+            builder: &mut builder,
+            variables: HashMap::new(),
+            module,
+        };
+
+        for (i, arg_type) in arguments.iter().enumerate() {
+            let val = function_compiler.builder.block_params(entry_block)[i];
+            let var = function_compiler.builder.declare_var(*arg_type);
+            function_compiler.builder.def_var(var, val);
+        }
 
         for stmt in code.statements {
-            Self::compile_stmt(&mut builder, &mut next_var, &mut variables, &stmt)?;
+            function_compiler.compile_stmt(&stmt)?;
         }
 
         builder.finalize();
 
         module
             .define_function(func_id, &mut ctx)
-            .map_err(|e| format!("{} {}", "Unable to define function:".red().bold(), e))?;
+            .map_err(|e| format!("Unable to define function: {e}"))?;
 
         module.clear_context(&mut ctx);
 
         Ok(func_id)
     }
+}
 
-    fn compile_expr(
-        builder: &mut FunctionBuilder,
-        variables: &HashMap<String, Variable>,
-        expr: &Expression,
-    ) -> Value {
+struct FunctionCompiler<'a, 'b: 'a, M: Module + ?Sized> {
+    builder: &'a mut FunctionBuilder<'b>,
+    variables: HashMap<String, Variable>,
+    #[allow(dead_code)]
+    module: &'a mut M,
+}
+
+impl<'a, 'b, M: Module + ?Sized> FunctionCompiler<'a, 'b, M> {
+    fn compile_expr(&mut self, expr: &Expression) -> Value {
         match expr {
-            Expression::Int(n) => builder.ins().iconst(types::I64, *n),
-            Expression::Float(n) => builder.ins().f64const(*n),
+            Expression::Int(n) => self.builder.ins().iconst(types::I64, *n),
+            Expression::Float(n) => self.builder.ins().f64const(*n),
             Expression::Boolean(b) => {
                 let v: i64 = if *b { 1 } else { 0 };
-                builder.ins().iconst(types::I8, v)
+                self.builder.ins().iconst(types::I8, v)
             }
             Expression::String(s) => todo!("strings ({s}) is not supported yet"),
             Expression::Identifier(name) => {
-                let var = variables
+                let var = self
+                    .variables
                     .get(*name)
                     .unwrap_or_else(|| panic!("Unknown variable {name}"));
-                builder.use_var(*var)
+                self.builder.use_var(*var)
             }
 
             Expression::Add { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder.ins().iadd(lho_compiled, rho_compiled)
+                let lhs = self.compile_expr(lho);
+                let rhs = self.compile_expr(rho);
+                match self.builder.func.dfg.value_type(lhs) {
+                    types::F64 => self.builder.ins().fadd(lhs, rhs),
+                    _ => self.builder.ins().iadd(lhs, rhs),
+                }
             }
             Expression::Sub { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder.ins().isub(lho_compiled, rho_compiled)
+                let lhs = self.compile_expr(lho);
+                let rhs = self.compile_expr(rho);
+                match self.builder.func.dfg.value_type(lhs) {
+                    types::F64 => self.builder.ins().fsub(lhs, rhs),
+                    _ => self.builder.ins().isub(lhs, rhs),
+                }
             }
             Expression::Mul { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder.ins().imul(lho_compiled, rho_compiled)
+                let lhs = self.compile_expr(lho);
+                let rhs = self.compile_expr(rho);
+                match self.builder.func.dfg.value_type(lhs) {
+                    types::F64 => self.builder.ins().fmul(lhs, rhs),
+                    _ => self.builder.ins().imul(lhs, rhs),
+                }
             }
             Expression::Div { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder.ins().sdiv(lho_compiled, rho_compiled)
+                let lhs = self.compile_expr(lho);
+                let rhs = self.compile_expr(rho);
+                match self.builder.func.dfg.value_type(lhs) {
+                    types::F64 => self.builder.ins().fdiv(lhs, rhs),
+                    _ => self.builder.ins().sdiv(lhs, rhs),
+                }
             }
             Expression::Mod { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder.ins().srem(lho_compiled, rho_compiled)
+                let lhs = self.compile_expr(lho);
+                let rhs = self.compile_expr(rho);
+                self.builder.ins().srem(lhs, rhs)
             }
 
             Expression::Neg { expr } => {
-                let expr_compiled = Self::compile_expr(builder, variables, expr);
-                let ty = builder.func.dfg.value_type(expr_compiled);
-
-                match ty {
-                    types::I64 => builder.ins().ineg(expr_compiled),
-                    types::F64 => builder.ins().fneg(expr_compiled),
-                    _ => panic!("Unary '-' is not supported for type {:?}", ty),
+                let val = self.compile_expr(expr);
+                match self.builder.func.dfg.value_type(val) {
+                    types::I64 => self.builder.ins().ineg(val),
+                    types::F64 => self.builder.ins().fneg(val),
+                    _ => panic!("Unary '-' not supported for this type"),
                 }
             }
 
-            Expression::Equal { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder.ins().icmp(IntCC::Equal, lho_compiled, rho_compiled)
-            }
-
-            Expression::NotEqual { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder
-                    .ins()
-                    .icmp(IntCC::NotEqual, lho_compiled, rho_compiled)
-            }
-
+            Expression::Equal { lho, rho } => self.compile_cmp(IntCC::Equal, lho, rho),
+            Expression::NotEqual { lho, rho } => self.compile_cmp(IntCC::NotEqual, lho, rho),
             Expression::Greater { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder
-                    .ins()
-                    .icmp(IntCC::SignedGreaterThan, lho_compiled, rho_compiled)
+                self.compile_cmp(IntCC::SignedGreaterThan, lho, rho)
             }
-
             Expression::GreaterEqual { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder
-                    .ins()
-                    .icmp(IntCC::SignedGreaterThanOrEqual, lho_compiled, rho_compiled)
+                self.compile_cmp(IntCC::SignedGreaterThanOrEqual, lho, rho)
             }
-
-            Expression::Less { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder
-                    .ins()
-                    .icmp(IntCC::SignedLessThan, lho_compiled, rho_compiled)
-            }
-
+            Expression::Less { lho, rho } => self.compile_cmp(IntCC::SignedLessThan, lho, rho),
             Expression::LessEqual { lho, rho } => {
-                let lho_compiled = Self::compile_expr(builder, variables, lho);
-                let rho_compiled = Self::compile_expr(builder, variables, rho);
-                builder
-                    .ins()
-                    .icmp(IntCC::SignedLessThanOrEqual, lho_compiled, rho_compiled)
+                self.compile_cmp(IntCC::SignedLessThanOrEqual, lho, rho)
             }
 
             Expression::Not { expr } => {
-                let expr_compiled = Self::compile_expr(builder, variables, expr);
-                builder.ins().bxor_imm(expr_compiled, 1)
+                let val = self.compile_expr(expr);
+                self.builder.ins().bxor_imm(val, 1)
             }
-
-            _ => todo!(),
         }
     }
 
-    fn compile_stmt(
-        builder: &mut FunctionBuilder,
-        next_var: &mut usize,
-        variables: &mut HashMap<String, Variable>,
-        stmt: &Statement,
-    ) -> Result<bool, String> {
+    fn compile_cmp(&mut self, cc: IntCC, lho: &Expression, rho: &Expression) -> Value {
+        let lhs = self.compile_expr(lho);
+        let rhs = self.compile_expr(rho);
+
+        let ty = self.builder.func.dfg.value_type(lhs);
+        if ty == types::F64 {
+            let float_cc = match cc {
+                IntCC::Equal => FloatCC::Equal,
+                IntCC::NotEqual => FloatCC::NotEqual,
+                IntCC::SignedGreaterThan => FloatCC::GreaterThan,
+                IntCC::SignedGreaterThanOrEqual => FloatCC::GreaterThanOrEqual,
+                IntCC::SignedLessThan => FloatCC::LessThan,
+                IntCC::SignedLessThanOrEqual => FloatCC::LessThanOrEqual,
+                _ => FloatCC::Equal,
+            };
+            self.builder.ins().fcmp(float_cc, lhs, rhs)
+        } else {
+            self.builder.ins().icmp(cc, lhs, rhs)
+        }
+    }
+
+    fn compile_stmt(&mut self, stmt: &Statement) -> Result<bool, String> {
         match stmt {
             Statement::Let { name, value } => {
-                let let_compiled = Self::compile_expr(builder, variables, value);
+                let val = self.compile_expr(value);
+                let ty = self.builder.func.dfg.value_type(val);
 
-                let var = Self::alloc_var(builder, next_var, variables, name);
-                builder.def_var(var, let_compiled);
+                let var = self.builder.declare_var(ty);
+                self.variables.insert(name.to_string(), var);
+
+                self.builder.def_var(var, val);
                 Ok(false)
             }
             Statement::Ret { value } => {
-                let ret_compiled = Self::compile_expr(builder, variables, value);
-                builder.ins().return_(&[ret_compiled]);
+                let val = self.compile_expr(value);
+                self.builder.ins().return_(&[val]);
                 Ok(true)
             }
             Statement::If {
@@ -231,77 +238,60 @@ impl IRCompiler {
                 then_branch,
                 else_branch,
             } => {
-                let condition_val = Self::compile_expr(builder, variables, condition);
+                let condition_val = self.compile_expr(condition);
 
-                let then_block = builder.create_block();
-                let merge_block = builder.create_block();
-
+                let then_block = self.builder.create_block();
+                let merge_block = self.builder.create_block();
                 let else_block = if else_branch.is_some() {
-                    builder.create_block()
+                    self.builder.create_block()
                 } else {
                     merge_block
                 };
 
-                builder
+                self.builder
                     .ins()
                     .brif(condition_val, then_block, &[], else_block, &[]);
 
                 // then
-                builder.switch_to_block(then_block);
-                builder.seal_block(then_block);
+                self.builder.switch_to_block(then_block);
+                self.builder.seal_block(then_block);
 
                 let mut then_terminated = false;
-                for stmt in &then_branch.statements {
-                    if Self::compile_stmt(builder, next_var, variables, stmt)? {
+                for s in &then_branch.statements {
+                    if self.compile_stmt(s)? {
                         then_terminated = true;
                         break;
                     }
                 }
-
                 if !then_terminated {
-                    builder.ins().jump(merge_block, &[]);
+                    self.builder.ins().jump(merge_block, &[]);
                 }
 
                 // else
                 if let Some(else_branch_content) = else_branch {
-                    builder.switch_to_block(else_block);
-                    builder.seal_block(else_block);
+                    self.builder.switch_to_block(else_block);
+                    self.builder.seal_block(else_block);
 
-                    let mut then_terminated = false;
-                    for stmt in &else_branch_content.statements {
-                        if Self::compile_stmt(builder, next_var, variables, stmt)? {
-                            then_terminated = true;
+                    let mut else_terminated = false;
+                    for s in &else_branch_content.statements {
+                        if self.compile_stmt(s)? {
+                            else_terminated = true;
                             break;
                         }
                     }
-
-                    if !then_terminated {
-                        builder.ins().jump(merge_block, &[]);
+                    if !else_terminated {
+                        self.builder.ins().jump(merge_block, &[]);
                     }
                 }
 
                 // merge
-                builder.switch_to_block(merge_block);
-                builder.seal_block(merge_block);
+                self.builder.switch_to_block(merge_block);
+                self.builder.seal_block(merge_block);
 
                 Ok(false)
             }
-
-            _ => todo!(),
+            Statement::Fn { .. } => Err("Nested functions are not supported".to_string()),
         }
-    }
-
-    fn alloc_var(
-        builder: &mut FunctionBuilder,
-        next_var: &mut usize,
-        variables: &mut HashMap<String, Variable>,
-        name: &str,
-    ) -> Variable {
-        // NOTE: change I64 to something later
-        let var = builder.declare_var(types::I64);
-        *next_var += 1;
-        variables.insert(name.to_string(), var);
-        var
     }
 }
 
@@ -310,6 +300,6 @@ pub fn translate(t: &crate::parser::Type) -> cranelift::prelude::Type {
         crate::parser::Type::Int => types::I64,
         crate::parser::Type::Float => types::F64,
         crate::parser::Type::Boolean => types::I8,
-        crate::parser::Type::String => todo!("do not know"),
+        crate::parser::Type::String => todo!("String types not implemented yet"),
     }
 }
